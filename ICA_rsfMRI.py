5dd2735d566a4169ab5908a35e6de7a262cf3f9b
Pavithran Giriprakash <pavigiri1998@gmail.com>
2022-01-05 16:23:03 -0800

    End of the day update
    Have completed the code until the ICA component saving stage. The dual regression concept is completed. Need to include a saving function that saves each of the components in the patient specific directory.  Also, uploading a script that serves as my script for trying out all the functions apriori before finalization.

--- ICA_rsfMRI.py
+++ ICA_rsfMRI.py
@@ -4,40 +4,8 @@ import matplotlib.pyplot as plt
 import nibabel as nib
 from sklearn.decomposition import PCA
 from sklearn.decomposition import FastICA
-from scipy.io import savemat
-# CONCATENATION OF NII FILES
-def nii_concat(input_path, save_path):
-    for dirpath,dirs, filenames in os.walk(input_path):
-        for subdir in dirs:
-            niilist = []
-            subpath = os.path.join(input_path, subdir)
-            if os.path.exists(subpath):
-                os.chdir(subpath)
-            else:
-                print("Current working directory doesn't exist")
-            for files in os.listdir():
-                if files.endswith(".nii"):
-                    niilist.append(files)
-            imgcat = nib.concat_images(niilist)
-            nib.save(imgcat, os.path.join(save_path, 'MNI-' + subdir + '.nii'))
-            del imgcat
-#
-# OBTAINING THE NII IMAGE DIMENSIONS FOR LATER USE
-def _getnii_details(location,filename):
-    import os
-    import nibabel as nib
-    import numpy as np
-    if os.path.exists(location):
-        os.chdir(location)
-    else:
-        print("Current working directory doesn't exist")
-    img = nib.load(filename)
-    data = img.get_fdata()
-    n_voxels = np.prod(data.shape[:-1])
-    n_trs = data.shape[-1]
-    vols_shape = data.shape[:-1]
-    nii_dim = data.shape
-    return nii_dim,vols_shape,n_voxels,n_trs
+import scipy as sp
+
 # CENTERING BASED ON MEANS
 def center_mean(x):
     mean = np.mean(x, axis=1, keepdims=True)
@@ -59,22 +27,8 @@ def whiten(x):
     Xw = np.dot(whiteM, x)
     print('Whitening operation - DONE')
     return Xw, whiteM
-# DO PCA ANALYSIS
-def _do_PCA (x,n_components):
-    # Centering the data
-    x_cen = center_mean(x)
-    # Calculation of covariance
-    cov_x = cov(x_cen[0])
-    # SVD decomposition to obtain eigenvalues and eigen vector
-    U, S, V = np.linalg.svd(cov_x)
-    # No need of sorting - SVD already orders S in descending order
-    # Building the eigen vector matrix
-    W = U[:,0:n_components]
-    # Projection of the PCA components to the original data matrix
-    pca = (np.dot(x.T,W)).T
-    return pca
-# DO TEMPORAL CONCATENATION OF THE DATA
-def temporal_concat(location,n_comp,n_vxl):
+# GET THE LIST OF NII FILES IN THE LOCATION
+def _get_list_of_nii(location):
     if os.path.exists(location):
         os.chdir(location)
     else:
@@ -83,42 +37,54 @@ def temporal_concat(location,n_comp,n_vxl):
     for files in os.listdir():
         if files.endswith(".nii"):
             list_of_nii.append(files)
+    return list_of_nii
+# GET THE DETAILS OF A TEMPLATE NII IMAGE
+def _getnii_details(location,filename):
+    if os.path.exists(location):
+        os.chdir(location)
+    else:
+        print("Current working directory doesn't exist")
+    img = nib.load(filename)
+    data = img.get_fdata()
+    n_voxels = np.prod(data.shape[:-1])
+    n_trs = data.shape[-1]
+    vols_shape = data.shape[:-1]
+    nii_dim = data.shape
+    aff = img.affine
+    return nii_dim,vols_shape,n_voxels,n_trs,aff
+# CONCATENATION OF TRS TO GET 4D NIFTI FILE
+def _nii_concat(input_path, save_path):
+    for dirpath,dirs, filenames in os.walk(input_path):
+        for subdir in dirs:
+            subpath = os.path.join(input_path, subdir)
+            niilist = _get_list_of_nii(subpath)
+            imgcat = nib.concat_images(niilist)
+            nib.save(imgcat, os.path.join(save_path, 'MNI-' + subdir + '.nii'))
+            del imgcat
+# GENERATE 2D VOXEL-TIME SERIES DATA FROM 4D NIFTI IMAGE
+def _obtain_vt_data(nii_image):
+    n_voxels = np.prod(nii_image.shape[:-1])
+    n_trs = nii_image.shape[-1]
+    data = nii_image.get_fdata()
+    vt_data = (data.reshape((n_voxels, n_trs)))
+    return vt_data
+# DO PCA & TEMPORAL CONCATENATION OF THE DATA
+def _temporal_concat(location,n_comp,n_vxl):
+    list_of_nii = _get_list_of_nii(location)
     length = len(list_of_nii)
     tempcat_dat = np.empty([1,n_vxl])
     for i in range(length):
         pat_img = nib.load(list_of_nii[i])
-        n_voxels = np.prod(pat_img.shape[:-1])
-        n_trs = pat_img.shape[-1]
-        data = pat_img.get_fdata()
-        voxtime_dat = (data.reshape((n_voxels, n_trs))).T
+        voxtime_dat = _obtain_vt_data(pat_img).T
         print("Performing PCA using " + str(n_comp) + " components for subject " + str((i+1)) + "...")
-        #PCA_red = _do_PCA(voxtime_dat,n_comp)
-        PCA_bi = PCA(n_components=n_comp,whiten=True)
-        PCA_red = (PCA_bi.fit_transform(voxtime_dat.T)).T
+        PCA_red = _do_PCA_v2(voxtime_dat,n_comp)
         print("Performing temporal concatenation of subject " + str((i+1)) + "...")
         tempcat_dat = np.append(tempcat_dat, PCA_red, axis=0)
         del pat_img,n_voxels,n_trs,data, voxtime_dat
     print('Temporal concatenation -- DONE')
     tempcat_dat = np.delete(tempcat_dat,0,0)
     return tempcat_dat
-# DO GROUP ICA
-def _do_ICA(threshold,x,n_comp,z_score = None,thresh_method='min'):
-    from sklearn.decomposition import FastICA
-    ICA = FastICA(n_components=n_comp,whiten=False, algorithm="parallel",max_iter=1000)
-    ica = ICA.fit_transform(x.T).T
-    if z_score == True:
-        ica -= ica.mean(axis=0)
-        ica /= ica.std(axis=0)
-    else:
-        pass
-    if thresh_method == 'min':
-        ica[np.abs(ica)<threshold]=0
-    elif thresh_method == 'max':
-        ica[np.abs(ica)>threshold]=0
-    else:
-        pass
-    return ica
-# Plotting function to decide on the number of principal components
+# PLOT TO DECIDE ON THE NUMBER OF PCA COMPONENTS TO USE
 def _decide_PCA_comp (x):
     import numpy as np
     import matplotlib.pyplot as plt
@@ -133,8 +99,35 @@ def _decide_PCA_comp (x):
     plt.ylabel("Cumulative explained variance")
     plt.xticks(x_ax)
     plt.xlim([70,80])
+# DO PCA ANALYSIS
+def _do_PCA_v2(x,n_components):
+    pca = PCA(n_components=n_components,whiten=False)
+    pca_red = (pca.fit_transform(x.T)).T
+    return pca_red
+# OUTPUTTING THE RESULT OF ICA ANALYSIS AS NIFTI
+def _save_ica_nifti(mat_loc,mat_filename,img_affine,vol_shape,dest_loc):
+    os.chdir(mat_loc)
+    ica_mat = loadmat(str(mat_filename) + '.mat')
+    group_sm = ica_mat[mat_filename]
+    # CONVERTING TO Z-SCORE MAPS
+    group_sm -= group_sm.mean(axis=0)
+    group_sm /= group_sm.std(axis=0)
+    # THRESHOLDING IF NECESSARY
+    #group_sm[np.abs(group_sm) > thresh] = 0
+    # RESHAPING THE ICA MATRIX TO 4D
+    group_sm_4D = group_sm.T.reshape(vol_shape+(group_sm.shape[0], ))
+    for i in range(group_sm.shape[0]):
+        gica_comp = group_sm_4D[...,i]
+        gica_comp_img = nib.Nifti1Image(gica_comp,img_affine)
+        if not os.path.exists(dest_loc):
+            os.makedirs(dest_loc)
+        nib.save(gica_comp_img, os.path.join(dest_loc, 'gICA_component_' + str(i + 1) + '.nii'))
 
-dim,vol,vox,trs = _getnii_details(loc,file)
+
+    del gica_comp,gica_comp_img
+
+
+dim,vol,vox,trs,affi = _getnii_details(loc,file)
 path = 'C:/Users/PATTIAP/Desktop/Dataset/COBRE_fMRI_MNI'
 components = 20
 pca_tcat = temporal_concat(path, components,vox)
@@ -159,26 +152,54 @@ ICA_mat /= ICA_mat.std(axis=0)
 # THRESHOLDING THESE ICA MAPS
 ICA_mat[np.abs(ICA_mat) > 2] = 0
 # RESHAPING THESE BACK TO 4D NIFTI FILES
+i=0
 ICA_MAT_4D = ICA_mat.T.reshape(vol+(ICA_mat.shape[0],))
 for i in range(ICA_mat.shape[0]):
-    ICA_img = ICA_MAT_4D[:,:,:,i]
-ICA_MAT_4D[:,:,:,i]
+    ICA_img = ICA_MAT_4D[...,i]
+    ICA_comp_img = nib.Nifti1Image(ICA_MAT_4D,affi)
+    nib.
+
+
+ni_img = nib.Nifti1Image(ICA_MAT_4D,affi)
+nib.save(ni_img,os.path.join(path,'ICA_comp.nii'))
+del ICA_img
 # TRYING OUT DUAL REGRESSION
 # STEP 1 - SPATIAL REGRESSION
 # Using the template spatial maps as the
+def _dual_regression(location,gICA_sm):
+    gICA_sm -= gICA_sm.mean(axis=0)
+    gICA_sm /= gICA_sm.std(axis=0)
+    sublist = _get_list_of_nii(location)
+    number = len(sublist)
+    for i in range(number):
+        sub_img = nib.load(sublist[i])
+        sub_vt = _obtain_vt_data(sub_img)
+        sm = np.linalg.pinv(gICA_sm.T)
 
 
+ICA_mat -= ICA_mat.mean(axis=0)
+ICA_mat /= ICA_mat.std(axis=0)
 
 
+y = np.linalg.pinv(ICA_mat.T)
+ts = np.dot(y,voxtime_dat)
+ts = y*(voxtime_dat)
 
+# Trying out my ica function
 
+fileloc = 'C:/Users/PATTIAP/Desktop/Dataset/COBRE_fMRI_MNI'
+name = 'ica_red_mat'
+aff = img.affine
+desloc = 'C:/Users/PATTIAP/Desktop/COBRE_VF/Results/2.ICA/gICA_SM'
 
+_save_ica_nifti(fileloc,name,aff,vol,desloc)
 
-
-
-
-
-
+mat_loc = 'C:/Users/PATTIAP/Desktop/Dataset/COBRE_fMRI_MNI'
+mat_filename = 'ica_red_mat'
+img_affine = img.affine
+dest_loc = 'C:/Users/PATTIAP/Desktop/COBRE_VF/Results/2.ICA/gICA_SM'
+vol_shape = vol
+i=0
 
 
 
