37dca1849acda65eeeea2a2a0ef6afcb967b42a0
Pavithran Giriprakash <pavigiri1998@gmail.com>
2022-01-24 15:55:49 -0800

    End of the day update

--- IC_rsfMRI_matlab.m
+++ IC_rsfMRI_matlab.m
@@ -2,10 +2,9 @@
 clc
 clear
 tic
-
+% Getting the list of all the directories
 rootdir = 'E:\LRCBH\Data\COBRE-MNI\Individual_data';
-savedir = 'E:\LRCBH\Results\Matlab\1.PCA';
-%savedir = 'W:\';
+pca_savedir = 'E:\LRCBH\Results\Matlab\1.PCA';
 ica_savedir = 'E:\LRCBH\Results\Matlab\2.ICA';
 dirpath = dir(rootdir);
 subdir = [dirpath(:).isdir];
@@ -16,7 +15,10 @@ cd('E:\LRCBH\MNI_segmented');
 %cd('W:\MNI_segmented')
 m = load_untouch_nii('standard_binary.nii');
 M = m.img;
+nii_temp = m;
 [x, y, z] = size(M);
+mask_temp = reshape(M, [1,x*y*z]);
+[~, indices] = find(mask_temp);
 trs = 850;
 comp = 200;
 %Iterating through each of the subjects
@@ -33,13 +35,13 @@ for i=1:length(subloc)
     fprintf('Performing PCA reduction using %d components for subject %s...\n',comp,suboi);
     %Performing subject wise PCA reduction
     PCA_red = double(do_PCA(vt_data,comp));
-    cd(savedir);
-    save(fullfile(savedir, sprintf('PCA_%s.mat',suboi)),'PCA_red','vt_data','index');
+    cd(pca_savedir);
+    save(fullfile(pca_savedir, sprintf('PCA_%s.mat',suboi)),'PCA_red','vt_data','index');
 end
 fprintf('PCA reduction done.\n');
 % Doing temporal concatenation of the subjects
 fprintf('Performing temporal concatenation of subjects...\n');
-tcat_data = temporal_concat(subloc,savedir);
+tcat_data = temporal_concat(subloc,pca_savedir);
 fprintf('Temporal concatenation done.\n')
 clearvars -except tcat_data
 % Performing spatial ICA based on hyperbolic tangent
@@ -57,10 +59,60 @@ npca=30;
     ica_DC_improved(tcat_data,Sigma,method,eps,npca,A0,a1,var_normal,shift,determine_flip);
 save(fullfile(ica_savedir,sprintf('gica_%d_result.mat',npca)),'A','S','W','White','-v7.3');
 toc
-%
-% [~,idx] = find(M);
-% index=index';
-% PCA_red = double(do_PCA(Ydata,100));
-% pat = '008';
-% file = fullfile(savedir, sprintf('PCA_%s.mat', pat));
-% dat = load(file,'PCA_red');
+
+%Saving the individual ica components as nii files
+for j = 1:size(S,1)
+    temp = zeros(1, x*y*z);
+    ica_comp = S(j,:);
+    temp(1,indices) = ica_comp;
+    ica_3d = reshape(temp,[x,y,z]);
+    nii_temp = m;
+    nii_temp.img = [];
+    nii_temp.img = single(ica_3d);
+    Sname = ['ica_',sprintf(['%03d.nii'],j)];
+    save_untouch_nii(nii_temp,[ica_savedir,'\',Sname]);
+end
+% Doing dual regression
+cd(ica_savedir);
+ica_result=load('gica_30_result.mat');
+dr_savedir = 'E:\LRCBH\Results\Matlab\3.DR';
+pca_savedir = 'E:\LRCBH\Results\Matlab\1.PCA\Trial';
+S=ica_result.S;
+noise_idx = [2,4,11,13,14,17,18,19,28,23];
+%Using only the useful components
+S(noise_idx,:)=[];
+%Getting the directory having the subject wise voxel time data
+dirloc = dir(pca_savedir);
+subloc = {dirloc.name}';
+subloc(ismember(subloc,{'.','..'})) = [];
+for i =1:length(subloc)
+    suboi = subloc{i};
+    current = strcat(pca_savedir,'\', suboi);
+    sub_data = load(current,'vt_data');
+    sub_data = (sub_data.vt_data)';
+    %Performing spatial regression
+    ss_tc = (pinv(S))'*sub_data;
+    %Performing temporal regression
+    ss_sm = sub_data*pinv(ss_tc);
+    ss_dr_dir = strcat(dr_savedir,'\',suboi(5:7));
+    if ~exist(ss_dr_dir,'dir')
+        mkdir(ss_dr_dir);
+    end
+    save(fullfile(ss_dr_dir,sprintf('dualregression.mat')),'ss_tc',"ss_sm");
+    % Saving the independent components as nii images
+    save_ica_nii(ss_sm',x,y,z,indices,m,ss_dr_dir);
+end
+
+
+
+    i=1;
+
+    x = (pinv(ss_tc));
+    x=x';
+
+%List of inputs to the dual regression function
+% The group ICA map
+% the noise components indices
+% dual regression save dir
+% pca directory (voxel time data files)
+heatmap(R);
---
+++ save_ica_nii.m
@@ -0,0 +1,11 @@
+function save_ica_nii(ica_mat,xdim,ydim,zdim,idx,nii_temp,savepath)
+for j = 1:size(ica_mat,1)
+    temp = zeros(1, xdim*ydim*zdim);
+    ica_comp = ica_mat(j,:);
+    temp(1,idx) = ica_comp;
+    ica_3d = reshape(temp,[xdim,ydim,zdim]);
+    nii_temp.img = [];
+    nii_temp.img = single(ica_3d);
+    Sname = ['ica_',sprintf(['%03d.nii'],j)];
+    save_untouch_nii(nii_temp,[savepath,'\',Sname]);
+end
