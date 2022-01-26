8c4c156d5f3b37bca0427b3503786d39990116f7
Pavithran Giriprakash <pavigiri1998@gmail.com>
2022-01-25 16:51:25 -0800

    End of the day update
    Included a function for finding the significant associations. Something is wrong there - need to verify that.

---
+++ confound_sig.m
@@ -0,0 +1,31 @@
+function [coeff,pval_corr,stats_corrected,n_compar,sig_loc] = confound_sig(fcn_dir, X, var_name,corrmat_template)
+% Finding the non-zero networks
+corr_mat = tril(corrmat_template,-1);
+[row,col] = find(corr_mat);
+nz_loc = horzcat(row,col);
+% Loading the functional connectivity matrix
+dirloc = dir(fcn_dir);
+subloc = {dirloc.name}';
+subloc(ismember(subloc,{'.','..'})) = [];
+for i =1:length(subloc)
+    suboi = subloc{i};
+    current = strcat(fcn_dir,'\', suboi);
+    sub_data = load(current,var_name);
+    sub_data = (sub_data.fcn)';
+    corr_mat = tril(sub_data,-1);
+    corr_vec = nonzeros(corr_mat);
+    fnc_val{i,1} = corr_vec';
+end
+% Forming the Y data
+Y = vertcat(fnc_val{:});
+% Preforming regression
+for k = 1:size(Y,2)
+    [coeff(:,k),~,~,~,stats(:,k)] = regress(Y(:,k),X);
+end
+% Correcting for multiple comparisons
+n_compar = size(Y,2);
+stats_corrected = stats(3,:)/n_compar;
+pval_corr = 0.5/n_compar;
+sig_idx = find(stats_corrected<pval_corr);
+sig_loc = nz_loc(sig_idx);
+end
